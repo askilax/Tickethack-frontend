81f90d8c7abbb82dee1688adc5f0b21806e64f5b
Your name <Your email>
2024-09-25 15:08:56 +0200

    bouton

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelector('#btn-search').addEventListener('click', function () {
     const departure = document.getElementById('departure').value;
     const arrival = document.getElementById('arrival').value;
     const dateGet = document.getElementById('date').value;
-    const date=moment(dateGet).format('YYYY-MM-DD');
+    const date = moment(dateGet).format('YYYY-MM-DD');
     console.log(departure, arrival, date);
     fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
         .then(response => response.json())
@@ -14,40 +14,43 @@ document.querySelector('#btn-search').addEventListener('click', function () {
                 for (let i = 0; i < tripsCount; i++) {
                     const time = moment(data.trips[i].date).format('LT');
                     const price = data.trips[i].price;
-                    const dateReturn = data.trip[i].date;
                     document.querySelector('#search-results').innerHTML += ` 
         <div class="row-book">
-                <span class="departure">${departure}</span> <span> '>' </span><span class="arrival">${arrival}</span> 
+                <span class="departure">${departure}</span> <span> > </span><span class="arrival">${arrival}</span> 
                 <span class="time">${time}</span>
-                <span class="date">${dateReturn}</span>
                 <span class="price">${price} €</span>
-                <button id="btn-book">Book</button>
+                <button id=${data.trip[i]._id} class= >Book</button>
         </div>
       `;
-     }
-}else {
-    document.querySelector('#search-result').innerHTML += ` 
+                }
+            } else {
+                document.querySelector('#search-result').innerHTML += ` 
            <div id="default-message" class="centered">
                         <img src="images/train.png" alt="Train Icon" class="train-icon">
                         <p>No trip found.</p>
             </div>
           `
-}
+            }
 
-});
-;
+        });
+    ;
 });
 // envoi des trajets dans le panier
-/*document.querySelector('#btn-book').addEventListener('click', function () {
-    const departure = document.querySelector('.departure').textContent;
-    const arrival = document.querySelector('.arrival').textContent;
-    const date = document.querySelector('.date').textContent;
-    const dateGet = document.querySelector('.time').textContent;
-    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
+document.querySelector('#btn-book').addEventListener('click', function () {
+    const ID = this.id;
+    const isPaid=false;
+
+    const data = { departure, arrival, date, price, isPaid };
+    console.log(data)
+
+    fetch('http://localhost:3000/bookings', {
+        method: "POST",
+	    headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
         .then(response => response.json())
-        .then(data => {
-    
-*/
+});
+
 
 
 //affichage des trajets dans le panier
